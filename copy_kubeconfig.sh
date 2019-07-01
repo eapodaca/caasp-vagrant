ec68f2ec8725386ca951f7caf77d88e5ace82b5a
Ethan Apodaca <eapodaca@suse.com>
2019-07-01 13:06:31 -0700

    Fix insecure tls because of hostname mismatch

--- copy_kubeconfig.sh
+++ copy_kubeconfig.sh
@@ -8,13 +8,12 @@ scp -F .vagrant/ssh-config -r deployer:/home/vagrant/.kube .caasp/kube
 
 master_ip=`grep -A1 'Host caasp-master-1' .vagrant/ssh-config | grep HostName | awk '{ print $2 }'`
 
-ca=`base64 .caasp/kube/SUSE_CaaSP_CA.crt -w 99999999999999`
-client_cert=`base64 .caasp/kube/kubectl-client-cert.crt -w 99999999999999`
-client_key=`base64 .caasp/kube/kubectl-client-cert.key -w 99999999999999`
+client_cert=`base64 .caasp/kube/kubectl-client-cert.crt -w 0`
+client_key=`base64 .caasp/kube/kubectl-client-cert.key -w 0`
 
 sed -i 's/\/home\/vagrant\/.kube\///' .caasp/kube/config
 sed -i "s/caasp-master-1/${master_ip}/" .caasp/kube/config
 
-sed -i "s/^\s*certificate-authority:.*$/    certificate-authority-data: ${ca}/m" .caasp/kube/config
+sed -i "s/^\s*certificate-authority:.*$/    insecure-skip-tls-verify: true/m" .caasp/kube/config
 sed -i "s/^\s*client-certificate:.*$/    client-certificate-data: ${client_cert}/m" .caasp/kube/config
 sed -i "s/^\s*client-key:.*$/    client-key-data: ${client_key}/m" .caasp/kube/config
